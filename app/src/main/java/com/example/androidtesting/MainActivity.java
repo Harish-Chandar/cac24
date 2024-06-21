6e714e986d56ce1145b562b934b59e8e9c1321d5
Svar Pandya <svarp02@gmail.com>
2024-06-21 18:23:56 -0500

    hello

--- app/src/main/java/com/example/androidtesting/MainActivity.java
+++ app/src/main/java/com/example/androidtesting/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidtesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> points = new ArrayList<>();
+
+        points.add("(24,3)");
+        points.add("(16,7)");
+        points.add("(20,4)");
+        points.add("(9,8)");
+        points.add("(21,5)");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
