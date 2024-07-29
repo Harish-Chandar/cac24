a3c3ddd154be2e0ff9bb2af5517a28d91bf10409
Harish <149600195+Harish-Chandar@users.noreply.github.com>
2024-07-28 20:00:38 -0500

    2d array + idea

--- app/src/main/java/com/example/androidtesting/MainActivity.java
+++ app/src/main/java/com/example/androidtesting/MainActivity.java
@@ -11,13 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String> points = new ArrayList<>();
+        
+        double[][] grid = new double[100][100];
 
-        points.add("(24,3)");
-        points.add("(16,7)");
-        points.add("(20,4)");
-        points.add("(9,8)");
-        points.add("(21,5)");
+        /**
+         * Use a 2D array for the map. Each double value in the grid is the probability of the criminal
+         * living there, which we will calculate and then set.
+         * The probability -1 will be at the locations the crimes were committed.
+         * The first index provided will be the column number, and the second the row number (xy system)
+         * (0,0) will be located in the top right.
+         * Use Pythagorean distance, not Manhattan distance.
+         */
+
+        grid[85][2] = -1; // this indicates a crime was committed at the location (85,2)
+        grid[16][34] = -1; // this indicates a crime was committed at the location (16,34)
 
 
 
